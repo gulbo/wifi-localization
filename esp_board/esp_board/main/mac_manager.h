7eb45b9fd4d8d44ee8efdd811ff19974c7600dad
Andrea Gulberti <and.gulberti@gmail.com>
2020-05-02 21:57:17 -0700

    add SniffedPacket class, comment mac_manager

--- esp_board/esp_board/main/mac_manager.h
+++ esp_board/esp_board/main/mac_manager.h
@@ -1,38 +1,38 @@
-#ifndef MAC_MANAGER_H
-#define MAC_MANAGER_H
+// #ifndef MAC_MANAGER_H
+// #define MAC_MANAGER_H
 
-#include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
-#include "esp_wifi_types.h"
-#include "string.h"
-#include "sys/types.h"
-#include "sys/socket.h"
-#include "sys/time.h"
-#include "netinet/in.h"
-#include "unistd.h"
-#include "errors.h"
+// #include "freertos/FreeRTOS.h"
+// #include "esp_wifi.h"
+// #include "esp_wifi_types.h"
+// #include "string.h"
+// #include "sys/types.h"
+// #include "sys/socket.h"
+// #include "sys/time.h"
+// #include "netinet/in.h"
+// #include "unistd.h"
+// #include "errors.h"
 
-#define MAX_MAC (15)
+// #define MAX_MAC (15)
 
-#if defined (__cplusplus)
-extern "C" {
-#endif
+// #if defined (__cplusplus)
+// extern "C" {
+// #endif
 
-typedef uint8_t** mac_list;
-typedef uint8_t* macflag_list;
+// typedef uint8_t** mac_list;
+// typedef uint8_t* macflag_list;
 
-extern uint8_t mac_stored[MAX_MAC][6];
-extern uint8_t flag_list[MAX_MAC];
-extern int nmacs;
+// extern uint8_t mac_stored[MAX_MAC][6];
+// extern uint8_t flag_list[MAX_MAC];
+// extern int nmacs;
 
-void init_mac_structures();
-int countEspFound(void);
-int cmp_mac(const wifi_promiscuous_pkt_t* pkt, uint8_t *mac);
-int recvmac(int s);
-void resetFlagList();
+// void init_mac_structures();
+// int countEspFound(void);
+// int cmp_mac(const wifi_promiscuous_pkt_t* pkt, uint8_t *mac);
+// int recvmac(int s);
+// void resetFlagList();
 
-#if defined (__cplusplus)
-}
-#endif
+// #if defined (__cplusplus)
+// }
+// #endif
 
-#endif
+// #endif
