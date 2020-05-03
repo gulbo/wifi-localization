7754e5c0e132c494f67115c9adeb52523a4d82db
Andrea Gulberti <and.gulberti@gmail.com>
2020-05-02 22:36:10 -0700

    better comments, remove unused headers

--- esp_board/esp_board/main/sniffed_packet.cpp
+++ esp_board/esp_board/main/sniffed_packet.cpp
@@ -1,6 +1,4 @@
 #include "sniffed_packet.h"
-//#include <iomanip>
-#include <string>
 #include <sstream>
 #include <chrono>
 
@@ -12,32 +10,37 @@ SniffedPacket::SniffedPacket(const wifi_promiscuous_pkt_t* wifi_pkt){
     memcpy(checksum, wifi_pkt->payload + wifi_pkt->rx_ctrl.sig_len - 4, 4);
 }
 
-SniffedPacket::SniffedPacket(uint8_t* mac_addr, uint8_t ssidLen, uint8_t* ssid, int16_t rssi, uint32_t timestamp, uint8_t* checksum){
-    memcpy(&(this->mac_addr), mac_addr, 6);
+SniffedPacket::SniffedPacket(uint8_t* mac_address,
+                             uint8_t ssid_length,
+                             uint8_t* ssid,
+                             int16_t rssi,
+                             uint32_t timestamp,
+                             uint8_t* checksum){
+    memcpy(&(this->mac_address), mac_address, 6);
     this->rssi = rssi;
-    memcpy(&(this->ssid), ssid, ssidLen);
+    memcpy(&(this->ssid), ssid, ssid_length);
     this->timestamp = timestamp;
     memcpy(&(this->checksum), checksum, 4);
-    this->ssid_len = ssidLen;
-    this->ssid[ssidLen] = '\0';
+    this->ssid_length = ssid_length;
+    this->ssid[ssid_length] = '\0';
 }
 
 std::string SniffedPacket::toString() const{
     std::stringstream ss;
     char mac[18];
-    snprintf(mac, 18, "%02x%02x%02x%02x%02x%02x", mac_addr[0],
-                                                       mac_addr[1],
-                                                       mac_addr[2],
-                                                       mac_addr[3],
-                                                       mac_addr[4],
-                                                       mac_addr[5]);
-    char cksum[12];
-    snprintf(cksum, 12, "%02x%02x%02x%02x", mac_addr[0],
-                                               mac_addr[1],
-                                               mac_addr[2],
-                                               mac_addr[3]);
+    snprintf(mac, 18, "%02x%02x%02x%02x%02x%02x", mac_address[0],
+                                                  mac_address[1],
+                                                  mac_address[2],
+                                                  mac_address[3],
+                                                  mac_address[4],
+                                                  mac_address[5]);
+    char cksum[12];  
+    snprintf(cksum, 12, "%02x%02x%02x%02x", mac_address[0],
+                                            mac_address[1],
+                                            mac_address[2],
+                                            mac_address[3]);
     ss << "MAC:" << mac << ",";
-    ss << "SSID_LENGTH:" << std::to_string(ssid_len) << ",";
+    ss << "SSID_LENGTH:" << std::to_string(ssid_length) << ",";
     ss << "SSID:" << ssid << ",";
     ss << "RSSI:" << rssi << ",";
     ss << "TIMESTAMP:" << timestamp << ",";
@@ -54,9 +57,9 @@ int SniffedPacket::send(int socket) const{
 
     //display();
 
-    //aggiungo mac_addr nel buffer
+    //aggiungo mac_address nel buffer
     printf("new pkt sent__________");
-    memcpy(buf_ptr, mac_addr, MAC_ADDRESS_BYTES);
+    memcpy(buf_ptr, mac_address, MAC_ADDRESS_BYTES);
     buf_ptr += MAC_ADDRESS_BYTES;
     printf("MAC_DST: %02x:%02x:%02x:%02x:%02x:%02x\n", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5]);
 
@@ -70,17 +73,17 @@ int SniffedPacket::send(int socket) const{
     printf("rssi reverse: %02d\n", (int16_t) buffer[6]);
 
     //NON trasmetto il terminatore
-    uint16_t networkd_ssid_len = htons(ssid_len);
+    uint16_t networkd_ssid_len = htons(ssid_length);
     memcpy(buf_ptr, &networkd_ssid_len, sizeof(uint16_t));
-    // tmp1 = htons(ssid_len);
+    // tmp1 = htons(ssid_length);
     // memcpy(buf_ptr, &tmp1, sizeof(uint16_t));
     buf_ptr += sizeof(uint16_t);
-    printf("ssid_len reverse: %02d\n", (int16_t) buffer[8]);
+    printf("ssid_length reverse: %02d\n", (int16_t) buffer[8]);
 
     //copio l'ssid nel buffer se la sua lunghezza è != 0
-    if(ssid_len != 0){
-        memcpy(buf_ptr, ssid, ssid_len);
-        buf_ptr += ssid_len;
+    if(ssid_length != 0){
+        memcpy(buf_ptr, ssid, ssid_length);
+        buf_ptr += ssid_length;
         *buf_ptr = 0;
         printf("ssid: %s\n", (char*) &(buffer[10]));
     }
@@ -89,12 +92,12 @@ int SniffedPacket::send(int socket) const{
     uint32_t network_timestamp = htonl((uint32_t) timestamp);
     memcpy(buf_ptr, &network_timestamp, sizeof(uint32_t));
     buf_ptr += sizeof(uint32_t);
-    printf("timestamp reverse: %u\n", (int16_t) buffer[10 + ssid_len]);
+    printf("timestamp reverse: %u\n", (int16_t) buffer[10 + ssid_length]);
 
     //copio il checksum nel buffer
     memcpy(buf_ptr, checksum, CHECKSUM_BYTES);
     buf_ptr += CHECKSUM_BYTES;
-    printf("FCS:%02x%02x%02x%02x\n", buffer[10 + ssid_len + 2], buffer[10 + ssid_len + 3], buffer[10 + ssid_len + 4], buffer[10 + ssid_len + 5]);
+    printf("FCS:%02x%02x%02x%02x\n", buffer[10 + ssid_length + 2], buffer[10 + ssid_length + 3], buffer[10 + ssid_length + 4], buffer[10 + ssid_length + 5]);
 
     //invio i dati sul socket connesso
     ssize_t result = ::send(socket, (void*) buffer, (size_t) (buf_ptr - buffer), 0);
@@ -116,16 +119,16 @@ int SniffedPacket::sendSniffedPacket(const SniffedPacket& packet, int socket){
 
 void SniffedPacket::setSSID_(const wifi_promiscuous_pkt_t* pkt){
     void* payload = (void*)(pkt->payload);
-    memcpy(&ssid_len, payload + 25, sizeof(ssid_len));
+    memcpy(&ssid_length, payload + 25, sizeof(ssid_length));
 
-    if(ssid_len != 0)
-        memcpy(ssid, payload + 26, ssid_len);
+    if(ssid_length != 0)
+        memcpy(ssid, payload + 26, ssid_length);
 
-    ssid[ssid_len] = '\0';
+    ssid[ssid_length] = '\0';
 }
 
 void SniffedPacket::setMAC_(const wifi_promiscuous_pkt_t* pkt){
-    memcpy(mac_addr, (void*)(pkt->payload) + 10, 6);
+    memcpy(mac_address, (void*)(pkt->payload) + 10, 6);
 }
 
 uint32_t SniffedPacket::getTimestamp_()
--- esp_board/esp_board/main/sniffed_packet.h
+++ esp_board/esp_board/main/sniffed_packet.h
@@ -1,21 +1,16 @@
 #ifndef __SNIFFED_PACKET_H__
 #define __SNIFFED_PACKET_H__
 
-#include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
 #include "esp_wifi_types.h"
-#include "string.h"
-#include "sys/types.h"
 #include "sys/socket.h"
-#include "sys/time.h"
-#include "netinet/in.h"
-#include "unistd.h"
 #include "errors.h"
 #include <iostream>
+#include <string>
 
 class SniffedPacket
 {
     public:
+        /** size constants **/
         const size_t MAC_ADDRESS_BYTES = 6;
         const size_t RSSI_BYTES  = 2; //da 1 byte lo estendo a 2 per poi fare la htons
         const size_t SSID_LENGTH_BYTES = 2;
@@ -23,26 +18,62 @@ class SniffedPacket
         const size_t TIMESTAMP_BYTES = 4;
         const size_t CHECKSUM_BYTES = 4;
 
-        SniffedPacket(uint8_t* mac_addr, uint8_t ssidLen, uint8_t* ssid, int16_t rssi, uint32_t timestamp, uint8_t* checksum);
+        /**
+         * @brief create a SniffedPacket passing the parameters
+         */
+        SniffedPacket(uint8_t* mac_address,
+                      uint8_t ssid_length,
+                      uint8_t* ssid,
+                      int16_t rssi,
+                      uint32_t timestamp,
+                      uint8_t* checksum);
 
+        /**
+         * @brief create a SniffedPacket from an esp32 packet
+         * @param wifi_pkt esp32 packet pointer
+         */
         SniffedPacket(const wifi_promiscuous_pkt_t* wifi_pkt);
         
+        /**
+         * @brief convert the packet to printable format
+         * @return
+         */
         std::string toString() const;
-
+        
+        /**
+         * @brief send the packet over a socket
+         * @param socket
+         */
         int send(int socket) const;
 
+        /**
+         * @brief send a given packet over a socket
+         * @param packet
+         * @param socket
+         */
         static int sendSniffedPacket(const SniffedPacket& packet, int socket);
         
     private:
-        uint8_t mac_addr[6];
-        uint8_t ssid_len;
+        uint8_t mac_address[6];
+        uint8_t ssid_length;
         uint8_t ssid[33];
         int16_t rssi;
         uint32_t timestamp;
         uint8_t checksum[4];
 
+        /** 
+         * @brief set the SSID starting from a raw packet pointer
+         */
         void setSSID_(const wifi_promiscuous_pkt_t* pkt);
+
+        /** 
+         * @brief set the MAC address starting from a raw packet pointer
+         */
         void setMAC_(const wifi_promiscuous_pkt_t* pkt);
+
+        /**
+         * @brief get the current epoch in seconds
+         */
         uint32_t getTimestamp_();
 };
 
