04b50a39b59b28611dc75eb0a38e2c676fbc9aa1
Andrea Gulberti <and.gulberti@gmail.com>
2020-05-19 18:53:49 -0700

    Add Client class, remove my_nvs, add cpp exceptions

---
+++ esp_board/esp_board/main/client.cpp
@@ -0,0 +1,353 @@
+#include "client.h"
+#include "esp_wifi.h"
+#include <iostream>
+
+const std::string ProtocolCode::HI = "HI";
+const std::string ProtocolCode::OK = "OK";
+const std::string ProtocolCode::RT = "RT";
+const std::string ProtocolCode::DE = "DE";
+const std::string ProtocolCode::GO = "GO";
+
+bool ProtocolCode::isValid(std::string code)
+{
+    if (code.size() != CODE_LENGTH)
+        return false;
+
+    return (code == HI || code == OK || code == RT || code == DE || code == GO);
+}
+
+Client::Client(uint8_t board_id, std::string server_ip, int16_t server_port){
+    board_id_ = board_id;
+    server_ip_ = server_ip;
+    server_port_ = server_port;
+
+    server_address_.sin_family = AF_INET;
+
+    if(inet_aton(server_ip_.c_str(), &(server_address_.sin_addr)) < 0){
+        throw std::runtime_error("Error parsing " + server_ip_);
+    }
+
+    server_address_.sin_port = htons(server_port);
+
+    std::cout << "****** Client Interface ******" << std::endl;
+    std::cout << "board_id: " << std::to_string(board_id) << std::endl;
+    std::cout << "server_ip: " << server_ip << std::endl;
+    std::cout << "server_port: " << std::to_string(server_port) << std::endl;
+    std::cout << "******************************" << std::endl;
+}
+
+void Client::close()
+{
+    if (is_connected_)
+    {
+        is_connected_ = false;
+        ::close(socket_);
+    }
+}
+
+Client::~Client()
+{
+    close();
+}
+
+bool Client::connect()
+{
+    if (!is_connected_)
+    {
+        socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (socket_ < 0)
+        {
+            throw std::runtime_error("Error creating the socket");
+        }
+
+        is_connected_ = true;
+        if (::connect(socket_, (struct sockaddr*) &(server_address_), sizeof(server_address_)) < 0)
+        {
+            close();
+        }
+    }
+
+    return is_connected_;
+}
+
+bool Client::isConnected()
+{
+    return is_connected_;
+}
+
+uint32_t Client::start()
+{
+    assert(is_connected_);
+
+    if (!sendHi_())
+        throw std::runtime_error("Error sending Hi");
+
+    // wait for OK reponse from the server
+    std::cout << "Waiting OK..." << std::endl;
+    std::string code;
+    if(!readProtocolCode_(code))
+        throw std::runtime_error("Invalid code received: " + code);
+    if(code != ProtocolCode::OK)
+        throw std::runtime_error("Waiting for OK, received : " + code);
+
+    std::cout << "Received OK" << std::endl;
+
+    // read how many boards are there
+    // should be always 0 for now
+    int32_t nmacs;
+    if(!readInt(nmacs))
+        throw std::runtime_error("Unexpected value received: " + std::to_string(nmacs));
+
+    if(nmacs != 0){
+        std::cout << "Error on nmacs! 0!=" << std::to_string(nmacs) << std::endl;
+        throw std::runtime_error("DE phase called but not implemented!");
+    }
+
+    // send DE 0
+    if(!sendDE_(0))
+        throw std::runtime_error("Error on sendDE");
+    
+    // wait for GO code
+    if(!readProtocolCode_(code))
+        throw std::runtime_error("Invalid code received: " + code);
+
+    if (code != ProtocolCode::GO)
+        throw std::runtime_error("Waiting for GO, received : " + code);
+
+    std::cout << "Received GO" << std::endl;
+
+    // wait for time from the server
+    uint32_t epoch = 0;
+    if(!readInt(epoch))
+    {
+        throw std::runtime_error("Error receiving time");
+    }
+
+    std::cout << "Time received: " << std::to_string(epoch) << std::endl;
+    return epoch;
+}
+
+bool Client::sendHi_(){
+    assert(is_connected_);
+
+    uint8_t mac_addr[SniffedPacket::MAC_ADDRESS_BYTES];
+    char buff[MAX_MESSAGE_LENGTH];
+
+    // TODO wrap this in a method in another class (like in a Wifi class maybe?)
+    esp_err_t err = esp_wifi_get_mac(ESP_IF_WIFI_STA, mac_addr);
+    switch(err){
+        case ESP_OK:
+            std::cout << "MAC_ADDR retrieved successfully" << std::endl;
+            break;
+        case ESP_ERR_WIFI_NOT_INIT:
+            throw std::runtime_error("esp_wifi_get_mac: WiFi not initialized!");
+        case ESP_ERR_WIFI_IF:
+            throw std::runtime_error("esp_wifi_get_mac: Invalid interface");
+        case ESP_ERR_INVALID_ARG:
+            throw std::runtime_error("esp_wifi_get_mac: Invalid argument");
+        default:
+            throw std::runtime_error("esp_wifi_get_mac: Unknown error");
+    }
+    // send [ HI IdBoard mac_addr ]
+    memcpy(buff, ProtocolCode::HI.c_str(), 2);
+    // TODO no sense to send idBoard as an int over the network (4 bytes)
+    // and have to do also all the htonl shit
+    // we should think about sending it as a char (1 byte).
+    int board_id_net = htonl(board_id_);
+    memcpy(&(buff[ProtocolCode::CODE_LENGTH]), &board_id_net, sizeof(board_id_net));
+    memcpy(&(buff[ProtocolCode::CODE_LENGTH + PROTO_NUM_LEN]), mac_addr, 6*sizeof(uint8_t));
+
+    int bytes_sent = ::send(socket_, buff, ProtocolCode::CODE_LENGTH + PROTO_NUM_LEN + 6, 0);
+    if(bytes_sent != ProtocolCode::CODE_LENGTH + PROTO_NUM_LEN + 6)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool Client::readProtocolCode_(std::string& code){
+    assert(is_connected_);
+
+    char msg[ProtocolCode::CODE_LENGTH];
+
+    size_t bytes_received = ::recv(socket_, msg, ProtocolCode::CODE_LENGTH, 0);
+    if(bytes_received != ProtocolCode::CODE_LENGTH){
+        std::cout << "Error reading protocol code" << std::endl;
+        return false;
+    }
+
+    code = std::string(msg);
+    return ProtocolCode::isValid(code);
+}
+
+bool Client::readInt(int32_t& value){
+    assert(is_connected_);
+    size_t bytes = ::recv(socket_, &value, sizeof(value), 0);
+    if(bytes != sizeof(value))
+    {
+        if (errno == EPIPE)
+        {
+            close();
+            throw std::runtime_error("Connection closed by the server");
+        }
+        return false;
+    }
+
+    value = ntohl(value);
+    return true;
+}
+
+bool Client::readInt(uint32_t& value){
+    assert(is_connected_);
+    size_t bytes = ::recv(socket_, &value, sizeof(value), 0);
+    if(bytes != sizeof(value))
+    {
+        if (errno == EPIPE)
+        {
+            close();
+            throw std::runtime_error("Connection closed by the server");
+        }
+        return false;
+    }
+
+    value = ntohl(value);
+    return true;
+}
+
+bool Client::sendInt(const uint32_t& value)
+{
+    assert(is_connected_);
+
+    uint32_t value_host = ntohl(value);
+    size_t bytes = ::send(socket_, (void *) &value_host, sizeof(value_host), 0);
+
+    if(bytes != sizeof(value_host))
+    {
+        if (errno == EPIPE)
+        {
+            close();
+            throw std::runtime_error("Connection closed by the server");
+        }
+        return false;
+    }
+    return true;
+}
+
+bool Client::sendDE_(int32_t esp_found){
+    assert(is_connected_);
+    uint8_t buff[MAX_MESSAGE_LENGTH];
+
+	printf("Sending DE %d to server\n", esp_found);
+    memcpy(buff, ProtocolCode::DE.c_str(), ProtocolCode::CODE_LENGTH);
+    int32_t esp_found_net = htonl(esp_found);
+    memcpy(&(buff[ProtocolCode::CODE_LENGTH]), &esp_found_net, sizeof(esp_found_net));
+    size_t message_length = ProtocolCode::CODE_LENGTH + sizeof(esp_found_net);
+
+    int res = ::send(socket_, buff, message_length, 0);
+    if(res != message_length){
+        return false;
+    }
+
+    return true;
+}
+
+bool Client::sendBoardID()
+{
+    return sendInt(board_id_);
+}
+
+void Client::sendPackets(std::list<SniffedPacket>& packets)
+{
+    assert(is_connected_);
+
+    // first send the total number of packets
+    uint32_t packets_count = ntohl(packets.size());
+    size_t bytes_sent = ::send(socket_, (void*) &packets_count, sizeof(packets_count), 0);
+    if (bytes_sent != sizeof(packets_count))
+    {
+        std::cout << "Error sending number of available packets" << std::endl;
+        std::cout << "Retrying at the next loop..." << std::endl;
+        return;
+    }
+
+    // then send all the packets one by one
+    uint32_t failed_count = 0;
+    for (auto packet_it = std::begin(packets); packet_it != std::end(packets); )
+    {
+        std::cout << "Sending packet... ";
+        // if successful delete the packet
+        if (sendPacket(*packet_it))
+        {
+            packet_it = packets.erase(packet_it);
+            std::cout << "SUCCESS" << std::endl;
+        }
+        else
+        {
+            ++packet_it;
+            ++failed_count;
+            std:: cout << "FAILED" << std::endl;
+        }
+    }
+
+    std::cout << "Send packets complete." << std::endl;
+    std::cout << "Total packets: " << std::to_string(packets_count) <<  std::endl;
+    std::cout << "Failed: " << std::to_string(failed_count) <<  std::endl;
+}
+
+bool Client::sendPacket(const SniffedPacket& packet)
+{
+    assert(is_connected_);
+
+    uint8_t buffer[SniffedPacket::MAC_ADDRESS_BYTES + 
+                   SniffedPacket::RSSI_BYTES +
+                   SniffedPacket::SSID_LENGTH_BYTES +
+                   SniffedPacket::SSID_BYTES +
+                   SniffedPacket::TIMESTAMP_BYTES +
+                   SniffedPacket::CHECKSUM_BYTES];
+
+    uint8_t *buf_ptr = buffer;
+
+    memcpy(buf_ptr, packet.mac_address, SniffedPacket::MAC_ADDRESS_BYTES);
+    buf_ptr += SniffedPacket::MAC_ADDRESS_BYTES;
+
+
+    //aggiungo rssi nel buffer
+    uint16_t network_rssi = htons(packet.rssi); 
+    memcpy(buf_ptr, &network_rssi, sizeof(uint16_t));
+    buf_ptr += sizeof(uint16_t);
+
+    //NON trasmetto il terminatore
+    uint16_t networkd_ssid_len = htons(packet.ssid_length);
+    memcpy(buf_ptr, &networkd_ssid_len, sizeof(uint16_t));
+    buf_ptr += sizeof(uint16_t);
+
+    //copio l'ssid nel buffer se la sua lunghezza è != 0
+    if(packet.ssid_length != 0){
+        memcpy(buf_ptr, packet.ssid, packet.ssid_length);
+        buf_ptr += packet.ssid_length;
+        *buf_ptr = 0;
+    }
+
+    //copio timestamp nel buffer
+    uint32_t network_timestamp = htonl((uint32_t) packet.timestamp);
+    memcpy(buf_ptr, &network_timestamp, sizeof(uint32_t));
+    buf_ptr += sizeof(uint32_t);
+
+    //copio il checksum nel buffer
+    memcpy(buf_ptr, packet.checksum, SniffedPacket::CHECKSUM_BYTES);
+    buf_ptr += SniffedPacket::CHECKSUM_BYTES;
+
+    //invio i dati sul socket connesso
+    size_t bytes_sent = ::send(socket_, (void*) buffer, (size_t) (buf_ptr - buffer), 0);
+
+    if(errno == EPIPE){
+        is_connected_ = false;
+        std::runtime_error("Connection dropped, broken pipe");
+    }
+
+    if(bytes_sent != (size_t) (buf_ptr - buffer)){
+        return false;
+    }
+
+    return true;
+}
--- esp_board/esp_board/main/main.cpp
+++ esp_board/esp_board/main/main.cpp
@@ -5,119 +5,44 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-#include "esp_wifi.h"
 #include "esp_wifi_types.h"
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_event_loop.h"
-#include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "string.h"
 #include "wifi.h"
 #include <sys/time.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <time.h>
-#include "my_nvs.h"
 #include "sniffed_packet.h"
-#include <vector>
+#include "client.h"
+#include <time.h>
+#include <list>
 #include <string>
 #include <iostream>
 
-#define MAX_RETRY (15)
 #define SNIFFER_CHANNEL (1)
 #define STACK_SIZE (50 * 1024)
 #define SNIFFING_TIME_SEC (60)
-#define MAX_LEN (100)
-#define PROTO_HI ("HI")
-#define PROTO_HI_CODE (17)
-#define PROTO_OK ("OK")
-#define PROTO_OK_CODE (18)
-#define PROTO_RT ("RT")
-#define PROTO_RT_CODE (19)
-#define PROTO_DE ("DE")
-#define PROTO_DE_CODE (20)
-#define PROTO_GO ("GO")
-#define PROTO_GO_CODE (21)
-#define PROTO_MSG_LEN (2)
-#define PROTO_NUM_LEN (4)
-#define MAX_BOARDS (10)
 
 #define SERVER_IP "10.0.0.78"
 #define SERVER_PORT 7999
+#define BOARD_ID 1
 
-
-std::vector<SniffedPacket> sniffed_packets;
-
-typedef struct info_s{
-    int idBoard;
-    struct sockaddr_in server_addr;
-} info_t;
-
-
+std::list<SniffedPacket> sniffed_packets;
+Client client(BOARD_ID, SERVER_IP, SERVER_PORT);
 
 static void config();
 void createTask();
 void wifi_sniffer_handler(void *buff, wifi_promiscuous_pkt_type_t type);
 void sendTask(void *parameters);
 void print_task_state(eTaskState state);
-int initialize_global_info();
-int init_proto(int s);
-int sendHi(int s, int idBoard);
-int sendPN(int s);
-int readCode(int s);
-int recvInt(int s);
-int settimestamp(int s, int sec);
-int sendDE(int s, int espFound);
+bool setTime(uint32_t epoch);
 
-info_t global_info; /* stores ID of this board and Server IP address and port */
-int s; /* socket for the connection with the server */
-nvs_handle handle;
 TaskHandle_t send_task_handle;
 const TickType_t send_task_delay = SNIFFING_TIME_SEC * 1000 / portTICK_PERIOD_MS;
 
-/** @brief send all the array of packets sniffed
- *  first send [#packets]
- *  then send every packet of the array at a time
- */
-int array_send(int socket){
-
-    if(socket <= 0){
-        printf("Invalid socket (%d)\n", socket);
-        return INVALID_SOCKET;
-    }
-
-    int packets_count = ntohl(sniffed_packets.size());
-    int res = send(socket, (void*)&packets_count, sizeof(packets_count), 0);
-
-    for(int i = 0; i < sniffed_packets.size(); i++){
-        res = SniffedPacket::sendSniffedPacket(sniffed_packets[i], socket);
-
-        if(res < 0){
-            printf("Packet at index %d returned %d while sending\n", i, res);
-        }
-    }
-
-    return 0;
-}
-
 static void config(){
-    // get Board ID, Server IP, Server Port
-    printf("nvs_initialization..\n");
-    int res = my_nvs_init(&handle);
-    if(res < 0){
-        printf("Error on nvs_initialization\n");
-        printf("Terminating...\n");
-        exit(-1);
-    }
-
-    int result = initialize_global_info();
-    if(result < 0){
-        printf("Error %d\n", result);
-        exit(-1);
-    }
-
-    s = -1;
     printf("initializing tcip adapter and wifi config\n");
     wifi_config();
 
@@ -129,7 +54,7 @@ static void config(){
     xReturned = xTaskCreate(&sendTask, "sendTask", STACK_SIZE, &x, 1 /*priority*/, &send_task_handle);
     if(xReturned != pdPASS){
         printf("Could not allocate required memory to create sendTask!\n");
-        exit(-1);
+        exit(1);
     }
     
     eTaskState state = eTaskGetState(send_task_handle);
@@ -165,7 +90,8 @@ void wifi_sniffer_handler(void *buf, wifi_promiscuous_pkt_type_t type){
     std::cout << sniffed_packets.back().toString() << std::endl;
 }
 
-/** @brief the main task
+/**
+ * @brief the main task
  *  First initialize the protocol
  *  Then starts sniffing for send_task_delay
  *  At the end sends the sniffed packets and starts sniffing again
@@ -174,7 +100,26 @@ void wifi_sniffer_handler(void *buf, wifi_promiscuous_pkt_type_t type){
 void sendTask(void *parameters){
     // doesn't start until the config is conluded
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-    s = init_proto(s);
+
+    // try to connect to the server
+    for(uint16_t tries = 1; tries <= Client::MAX_CONNECTION_RETRIES && !client.isConnected(); tries++){
+
+        std::cout << "Trying to connect: " << std::to_string(tries) << std::endl;
+        client.connect();
+    }
+    
+    if(!client.isConnected()){
+        std::cout << "Failed to connect to the server" << std::endl;
+        exit(1);
+    }
+
+    std::cout << "Connected to the server!" << std::endl;
+
+    std::cout << "Starting client..." << std::endl;
+    uint32_t epoch = client.start();
+    if(!setTime(epoch))
+        throw std::runtime_error("Error setting the first time from the server");
+    std::cout << "Client Started!" << std::endl;
 
     // every send_task_delay send:
     // --> id_board
@@ -183,38 +128,36 @@ void sendTask(void *parameters){
     // <-- time from server
     while(true){
         // start sniffing
-        printf("New sniffing phase: starting data acquisition\n");
+        std::cout << "New sniffing phase: starting data acquisition" << std::endl;
         esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_handler);
         
         // sleep for sniffing
         vTaskDelay(send_task_delay);
 
-        printf("New send phase: interrupting data acquisition\n");
+        std::cout << "New send phase: interrupting data acquisition" << std::endl;
         esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_nullhandler);
 
-        int32_t id_board_net = ntohl(global_info.idBoard);
-        size_t sent = send(s, (void *) &id_board_net, (size_t) sizeof(int32_t), 0);
-        if(sent != sizeof(int)){
-            printf("Error on send: %d (%d)\n", sent, errno);
-            if(errno == EPIPE){
-                printf("Connection closed by server\n");
-            }
-            close(s);
-        }
-        sent = array_send(s);
-        if(sent < 0){
-            printf("Errore sulla send: %d\n", sent);
-            close(s);
-            exit(-1);
+        if (!client.sendBoardID())
+        {
+            std::cout << "Error sending Board ID" << std::endl;
+            exit(1);
         }
 
-        printf("Waiting for timestamp from server...\n");
-        sent = settimestamp(s, 0);
-        if(sent <= 0){
-            printf("Error on settimestamp (%d)\n", sent);
-        }
+        client.sendPackets(sniffed_packets);
 
-        sniffed_packets.clear();
+        std::cout << "Waiting to receive time from server..." << std::endl;
+        bool is_time_set = false;
+        uint32_t epoch;
+        if (client.readInt(epoch))
+        {
+            if(setTime(epoch))
+                is_time_set = true;
+        }   
+
+        if (is_time_set)
+            std::cout << "Time set: " << std::to_string(epoch) << std::endl; 
+        else
+            std::cout << "Error setting the time. Continuing using last time received" << std::endl;
     }
 }
 
@@ -238,300 +181,21 @@ void print_task_state(eTaskState state){
     }
 }
 
-/** @brief initialize global_info with:
- *      idBoard of this esp32
- *      server IP Address and port
+/**
+ *  @brief set the time received by the server as the time of the system
+ *         this is needed to synchronize all the esp boards
  */
-int initialize_global_info(){
-    int32_t id = -1, port;
-    char buffer[MAX_LEN];
-    char *result;
-
-    id = readnvs_i32(handle, ID_KEY);
-    if(id < 0){
-        return NVS_ERROR;
-    }
-
-    result = readnvs_str(handle, IP_KEY, buffer, MAX_LEN);
-    if(result == NULL || result != buffer){
-        return NVS_ERROR;
-    }
-    result = SERVER_IP;
-
-    port = readnvs_i32(handle, PORT_KEY);
-    port = SERVER_PORT;
-    if(port < 0){
-        return NVS_ERROR;
-    }
-
-    global_info.idBoard = id;
-    global_info.server_addr.sin_family = AF_INET;
-    if(inet_aton(buffer, &(global_info.server_addr.sin_addr)) < 0){
-        printf("Error on inet_aton");
-        exit(-1);
-    }
-    global_info.server_addr.sin_port = htons((int16_t) port);
-
-    printf("Initialization...\n");
-    printf("idBoard: %d\n", id);
-    printf("server_ip: %s\n", buffer);
-    printf("server_port: %d\n", port);
-    printf("________________________");
-
-    return 0;
-}
-
-/** @brief set the time received with the GO message
- *  this is needed to synchronize all the esp boards
- */
-int settimestamp(int s, int sec){
-    uint32_t timestamp;
+bool setTime(uint32_t epoch){
     struct timeval tval;
-    int result;
-
-    result = recv(s, (void*) &timestamp, sizeof(timestamp), 0);
-    if(result < 0){
-        printf("Error on receiving timestamp (%d)\n", errno);
-        return RECV_ERROR;
-    }
-    timestamp = ntohl(timestamp);
-    tval.tv_sec = timestamp;
+    tval.tv_sec = epoch;
     tval.tv_usec = 0;
-    result = settimeofday(&tval, NULL);
+
+    int16_t result = settimeofday(&tval, NULL);
     if(result != 0){
-        printf("Error on settimeofday\n");
-        return SETTIME_ERROR;
-    }
-    printf("Timestamp received: %ld\n", tval.tv_sec);
-    
-    return 1;
-}
-
-/** @brief initialize the protocol with the server
- *  1) send [HI boardid(4B) MAC_ADDR(6B)]
- *  2) rcv [OK #espboards list_of_mac_addr]
- *  3) sniffing phase: tries to sniff the esp boards. ends in 5 seconds OR when found all the boards
- *  4) send [DE #espboards_detected]
- *  5a) rcv [GO time]
- *  6a) rcv [RT] retry sniffing
- */
-int init_proto(int sock){
-    char buff[MAX_LEN];
-    int res = 0;
-    int count = 0;
-
-    // try to connect to the server
-    bool is_connected = false;
-    while(count < MAX_RETRY && !is_connected){
-        sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if(sock == INVALID_SOCKET || sock < 0){
-            printf("Error on socket\n");
-            exit(-1);
-        }
-
-        s = sock;
-        printf("Trying to connect: %d\n", count);
-        res = connect(sock, (struct sockaddr*) &(global_info.server_addr), sizeof(global_info.server_addr));
-        if(res < 0){
-            count++;
-            printf("Error on connect: %d\n", res);
-            close(sock);
-        }
-        else{
-            is_connected = true;
-        }
-    }
-    
-    if(count >= MAX_RETRY){
-        printf("Could not connect to server... rebooting the system...\n");
-        close(s);
-        exit(-1);
+        return false;
     }
 
-    printf("Connection to server established... socket: %d\n", s);
-    fflush(stdout);
-
-	printf("Init_proto start with socket: %d\n", s);
-
-    // send [HI idBoard mac_addr]
-	printf("Sending hi\n");
-    res = sendHi(s, global_info.idBoard);    
-    if(res < 0){
-        printf("Error on sending init_proto: HELLO (%d)\n", errno);
-        if(errno == EPIPE){
-            printf("Connection closed by server\n");
-        }
-        return SEND_ERROR;
-    }
-
-    // wait for OK reponse from the server
-    // read how many boards are there
-	printf("Read code\n");
-    int nmacs;
-    res = readCode(s);
-    if(res == PROTO_OK_CODE){
-        printf("Received OK from server... Parsing the list of devices\n");
-        nmacs = recvInt(s);
-
-        if((nmacs < 0) || (nmacs > 0)){
-            printf("Error on nmacs! 0!=%d\n", nmacs);
-            printf("DE phase not implemented!");
-            exit(1);
-            return RECV_ERROR;
-        }
-    }
-    else{
-        printf("Unknown message...\n");
-        exit(-1);
-        return PROTO_UNKNOWN_MSG;
-    }
-    int nFound = 0;
-    res = sendDE(sock, nFound);
-    if(res < 0){
-        printf("Error on sendDE (%d)\n", res);
-        exit(-1);
-    }
-    // read GO code
-    res = readCode(sock);
-    if(res == PROTO_GO_CODE){
-        printf("Received GO message\n");
-    }
-    else if(res == PROTO_RT_CODE){
-        printf("RT received... retry ESP identification\n");
-        printf("ERROR! ESP identification not implemented");
-        exit(1);
-    }
-    else{
-        printf("Unknown code received: %s\n", buff);
-        exit(1);
-        return PROTO_UNKNOWN_MSG;
-    }
-
-    printf("Waiting for timestamp...\n");
-    res = settimestamp(sock, 0);
-    if(res <= 0){
-        printf("Error on settimestamp (%d)\n", res);
-    }
-
-    return sock;
-}
-    
-/** send [ HI idBoard mac_addr ]
- */
-int sendHi(int s, int idBoard){
-    uint8_t mac_addr[6];
-    esp_err_t err;
-    char buff[MAX_LEN];
-
-    err = esp_wifi_get_mac(ESP_IF_WIFI_STA, mac_addr);
-    switch(err){
-        case ESP_ERR_WIFI_NOT_INIT:
-            printf("WiFi not initialized!\n");
-            exit(-1);
-            break;
-        case ESP_ERR_INVALID_ARG:
-            printf("Invalid argument\n");
-            exit(-1);
-            break;
-        case ESP_ERR_WIFI_IF:
-            printf("Invalid interface\n");
-            exit(-1);
-            break;
-        case ESP_OK:
-            printf("MAC_ADDR retrieved successfully\n");
-            break;
-        default:
-            printf("Unknown error\n");
-            exit(-1);
-            break;
-    }
-    // send [ HI IdBoard mac_addr ]
-    memcpy(buff, PROTO_HI, 2);
-    // TODO no sense to send idBoard as an int over the network (4 bytes)
-    // and have to do also all the htonl shit
-    // we should think about sending it as a char (1 byte).
-    int netIdBoard = htonl(idBoard);
-    memcpy(&(buff[PROTO_MSG_LEN]), &netIdBoard, sizeof(netIdBoard));
-    memcpy(&(buff[PROTO_MSG_LEN + PROTO_NUM_LEN]), mac_addr, 6*sizeof(uint8_t));
-
-    int res = send(s, buff, PROTO_MSG_LEN + PROTO_NUM_LEN + 6, 0);
-    if(res != PROTO_MSG_LEN + PROTO_NUM_LEN + 6)
-        res = SEND_ERROR;
-    return res;
-}
-
-/** @brief read a message from the socket and returns the code of the message
- */
-int readCode(int s){
-    char buff[PROTO_MSG_LEN + 1];
-    int res;
-
-    if(s < 0){
-        printf("Invalid socket received\n");
-        return -1;
-    }
-
-    res = recv(s, buff, PROTO_MSG_LEN, 0);
-    if(res != PROTO_MSG_LEN){
-        printf("Error on recv read code\n");
-        // TODO try with std exceptions to handle this shit
-        exit(-1);
-        return RECV_ERROR;
-    }
-
-    buff[PROTO_MSG_LEN] = '\0';
-	printf("Message received: %s\n", buff);
-    if(strcmp(buff, PROTO_HI) == 0)
-        return PROTO_HI_CODE;
-    else if(strcmp(buff, PROTO_OK) == 0)
-        return PROTO_OK_CODE;
-    else if(strcmp(buff, PROTO_RT) == 0)
-        return PROTO_RT_CODE;
-    else if(strcmp(buff, PROTO_DE) == 0)
-        return PROTO_DE_CODE;
-    else if(strcmp(buff, PROTO_GO) == 0)
-        return PROTO_GO_CODE;
-    return PROTO_UNKNOWN_MSG;    
-}
-
-/** @brief read a 4 bytes integer from the socket
- */
-int recvInt(int s){
-    int res, n;
-    res = 0;
-    n = -1;
-
-    res = recv(s, &n, sizeof(n), 0);
-    if(res != sizeof(n)){
-        printf("Unexpected value received: read %d B from socket (%d)\n", res, errno);
-        return RECV_ERROR;
-    }
-
-    n = ntohl(n);
-    return n;
-}
-
-/** @brief
- *  sends ["DE" #esp_found ]
- *  //TODO remove it when we change the protocol. Right now we cannot remove it yet 
- */
-int sendDE(int s, int32_t esp_found){
-    uint8_t buff[MAX_LEN];
-
-	printf("Sending DE %d to server\n", esp_found);
-    memcpy(buff, PROTO_DE, PROTO_MSG_LEN);
-    int32_t esp_found_net = htonl(esp_found);
-    memcpy(&(buff[PROTO_MSG_LEN]), &esp_found_net, sizeof(esp_found_net));
-    size_t message_length = PROTO_MSG_LEN + sizeof(esp_found_net);
-
-    int res = send(s, buff, message_length, 0);
-    if(res != message_length){
-        printf("Error on send (%d)\n", res);
-        exit(1);
-        return SEND_ERROR;
-    }
-
-    return res;
+    return true;
 }
 
 int main(){
