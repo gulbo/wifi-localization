bc1a227a33bce7354898c59b8e5dfd6a08bdbc0f
Andrea Gulberti <and.gulberti@gmail.com>
2020-05-19 18:53:49 -0700

    Add Client class, remove my_nvs, add cpp exceptions

---
+++ esp_board/esp_board/main/client.h
@@ -0,0 +1,141 @@
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
+
+#include "sys/socket.h"
+#include "sniffed_packet.h"
+#include <list>
+
+#define PROTO_NUM_LEN (4)
+#define PROTO_UNKNOWN_MSG -50
+
+/**
+ * @brief struct that holds the protocol specific constants and methods
+ */
+struct ProtocolCode
+{
+    static const std::string HI;
+    static const std::string OK;
+    static const std::string RT;
+    static const std::string DE;
+    static const std::string GO;
+    static const size_t CODE_LENGTH = 2;
+
+    /** 
+     * @brief check if a code is a valid protocol code
+     * @return is valid
+     */
+    static bool isValid(std::string code);
+
+};
+
+/**
+ * @brief Client class that groups all the client interface to the server
+ */
+class Client
+{
+    public:
+
+        static const uint16_t MAX_CONNECTION_RETRIES = 15;
+        static const size_t MAX_MESSAGE_LENGTH = 128;
+
+        Client(uint8_t board_id, std::string server_ip, int16_t server_port);
+
+        ~Client();
+
+        /** 
+         * @brief connect to the server
+         * @return is_connected
+         */
+        bool connect();
+
+        /**
+         * @brief close connection
+         */
+        void close();
+
+        /**
+         * @brief is connected to the server
+         * @return
+         */
+        bool isConnected();
+
+        /**
+        * @brief initialize the protocol with the server
+        *  1) send [HI boardid(4B) MAC_ADDR(6B)]
+        *  2) rcv [OK 0 <list_of_mac_addr>]
+        *  4) send [DE 0]
+        *  5a) rcv [GO time]
+        *  6a) rcv [RT] retry sniffing
+        */
+        uint32_t start();
+
+        /**
+         * @brief read a 4 bytes integer from the socket
+         * @return successful
+         */
+        bool readInt(int32_t& value);
+
+        /**
+         * @brief read a 4 bytes unsigned integer from the socket
+         * @return successful
+         */
+        bool readInt(uint32_t& value);
+
+        /**
+         * @brief send a 4 bytes unsigned integer on the socket
+         * @return successful
+         */
+        bool sendInt(const uint32_t& value);
+
+        /**
+         * @brief send the board_id
+         * @return successful
+         */
+        bool sendBoardID();
+
+        /**
+         * @brief send a list of packets
+         *        first send [#packets]
+         *        then send every packet one at a time
+         * @param packets list
+         */
+        void sendPackets(std::list<SniffedPacket>& packets);
+
+        /**
+         * @brief send a packet
+         * @param packet
+         * @return successful
+         */
+        bool sendPacket(const SniffedPacket& packet);
+    
+    private:
+        uint8_t board_id_;
+        std::string server_ip_;
+        int16_t server_port_;
+        struct sockaddr_in server_address_;
+        int16_t socket_ = -1;
+        bool is_connected_ = false;
+        
+        /**
+         * @brief send [ HI idBoard mac_addr ]
+         * @return successful
+         */
+        bool sendHi_();
+
+        /**
+         * @brief sends ["DE" #esp_found ]
+         *        //TODO remove it when we change the protocol. Right now we cannot remove it yet 
+         * @return successful
+         */
+        bool sendDE_(int32_t esp_found);
+
+        /**
+        * @brief read a message and returns its code\
+        * @param code output argument for the code
+        * @return successful
+        */
+        bool readProtocolCode_(std::string& code);
+        
+};
+
+#endif
