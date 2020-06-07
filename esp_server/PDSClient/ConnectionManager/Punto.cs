6b4f9404006905a6d787656539167121c646b6f2
Daniele Maglione <danielelavoro93@gmail.com>
2020-06-07 05:38:01 +0200

    Refactorint Point.cs
    New name: Punto.cs

---
+++ esp_server/PDSClient/ConnectionManager/Punto.cs
@@ -0,0 +1,21 @@
+﻿namespace PDSClient.ConnectionManager
+{
+    public class Punto
+    {
+
+        //Proprietà
+
+        public double Ascissa {get; set;}
+        public double Ordinata {get; set;}
+
+        public Punto(double x, double y) {
+            Ascissa = x;
+            Ordinata = y;
+        }
+
+        public override string ToString()
+        {
+            return "Ascissa (X): " + Ascissa + " - " + "Ordinata (Y): " + Ordinata;
+        }
+    }
+}
