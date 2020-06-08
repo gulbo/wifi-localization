cfb8f358340183f055e07a1f914888a249e7707f
Andrea Gulberti <and.gulberti@gmail.com>
2020-06-08 20:38:33 +0200

    move wifi.c to cpp

--- esp_client/main/wifi.h
+++ esp_client/main/wifi.h
@@ -2,31 +2,13 @@
 #define WIFI_H
 
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/timers.h"
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
 #include "esp_wifi_types.h"
 #include "esp_system.h"
 #include "esp_event.h"
-#include "esp_event_loop.h"
-#include "string.h"
 
-#define SSID "GULPO"
-#define PASSWORD "f117f117bagonghi"
-
-#if defined (__cplusplus)
-extern "C" {
-#endif
-
-EventGroupHandle_t wifi_event_group;
-
-void wifi_config();
-void wifi_sniffer_nullhandler(void *buff, wifi_promiscuous_pkt_type_t type);
-void wifi_sniffer_set_channel(uint8_t channel);
-
-#if defined (__cplusplus)
-}
-#endif
+void initializeWifi(const char* wifi_ssid, const char* wifi_password, uint8_t wifi_channel);
+void wifiSnifferNullHandler(void *buff, wifi_promiscuous_pkt_type_t type);
 
 #endif
