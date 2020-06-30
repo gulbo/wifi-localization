cff40b48b0f3d40cbaa49663d17c93d4f477cedf
Daniele Maglione <danielelavoro93@gmail.com>
2020-06-30 14:44:16 +0200

    Refactoring PhoneInfo.cs
    New name: DatiDispositivo.cs

---
+++ esp_server/PDSClient/ConnectionManager/DatiDispositivo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PDSClient.ConnectionManager
+{
+    public class DatiDispositivo
+    {
+        private readonly String MAC;
+        private readonly Punto posizione;
+        private readonly int timestamp;
+        private readonly bool global;
+
+        public String MAC_Address
+        {
+            get { return MAC; }
+        }
+
+        public Punto Posizione
+        {
+            get { return posizione; }
+        }
+
+        public int Timestamp
+        {
+            get { return timestamp; }
+        }
+
+        public bool Global
+        {
+            get { return global; }
+        }
+
+        public DatiDispositivo(String mac, int timestamp, Punto posizione)
+        {
+            this.MAC = mac;
+            this.timestamp = timestamp;
+            this.posizione = posizione;
+        }
+
+        public DatiDispositivo(String mac, int timestamp, double x, double y)
+        {
+            this.MAC = mac;
+            this.timestamp = timestamp;
+            this.posizione = new Punto(x, y);
+        }
+
+        public DatiDispositivo(String mac, int timestamp, Punto posizione,  bool global)
+        {
+            this.MAC = mac;
+            this.posizione = posizione;
+            this.timestamp = timestamp;
+            this.global = global;
+        }
+        public DatiDispositivo(String mac, int timestamp, double x, double y, bool global)
+        {
+            this.MAC = mac;
+            this.posizione = new Punto(x, y);
+            this.timestamp = timestamp;
+            this.global = global;
+        }
+
+        public override string ToString()
+        {
+            return MAC + " - " + timestamp + " - " + "(" + posizione.Ascissa + ", " + posizione.Ordinata + ")";
+        }
+    }
+}
